4a0d0a8f25ad0c81b2aeb3a592e3092e6f97f0aa
XYX <85141301+jokerxyxx@users.noreply.github.com>
2022-06-02 16:01:03 +0800

    Add files via upload

---
+++ utils/utils.py
@@ -0,0 +1,17 @@
+import torch
+import numpy as np
+from PIL import Image
+import numpy as np
+import os
+
+def mkdirs(paths):
+    if isinstance(paths, list) and not isinstance(paths, str):
+        for path in paths:
+            mkdir(path)
+    else:
+        mkdir(paths)
+
+def mkdir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
